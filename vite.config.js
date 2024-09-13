f5fe2692628d2f2869692c7ecd17608a34efd4d5
unknown <iasafianu@st.ug.edu.gh>
2024-09-13 16:52:48 +0000

    onfig

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const backendAPI = import.meta.env.VITE_API_URL || "http://localhost:4000";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: `${backendAPI}`,
+        target: "http://localhost:4000",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
