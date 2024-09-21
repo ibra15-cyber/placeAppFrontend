ec097543ea0b87fbe8e9d7de928f6a35b6d5e1db
unknown <iasafianu@st.ug.edu.gh>
2024-09-21 17:51:25 +0000

    remove ucreated by

--- src/screens/PlaceDetailsScreen.jsx
+++ src/screens/PlaceDetailsScreen.jsx
@@ -84,7 +84,7 @@ const PlaceDetailsScreen = () => {
             </h3>
             <p>Latitude: {place.location.lat}</p>
             <p>Longitude: {place.location.lng}</p>
-            <p>Created by: {userLoggedIn.name}</p>
+            {/* <p>Created by: {userLoggedIn.name}</p> */}
             <p>Created at: {new Date(place.createdAt).toLocaleDateString()}</p>
           </div>
         )}
